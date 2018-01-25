9a3a6d6c19c017182446d23ab91c034a8dd9352a
JHNiemeijer <jh.niemeijer@online.nl>
2018-01-25 21:49:36 +0100

    ad argument

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-main() {}
+#include <iostream>
+
+/// This program shows arugments
+int main(int argc, char* argv[])
+{
+  for (int i=0; i!=argc; ++i)
+  {
+      std::cout << i << ": " << argv[i] << '\n';
+  }
+  if (std::string(argv[1]) == "--help")
+  {
+      std::cout << "HELP\n";
+  }
+}
